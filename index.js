da17f0b775b558ecb4c8350825da9f7a9bda1189
Mike Marquez <mike.marquez11@gmail.com>
2023-06-25 17:02:12 -0500

    Entrega Ejercicio Final Modulo 8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require("path");
 var port = process.env.PORT || 3000;
-
+var counter = 0;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -16,16 +16,22 @@ app.get("*", function(req,res,next) {
 io.on('connection', function(socket){
 	const user = socket.handshake.query.name;
 	const from = socket.id;
+	counter++;
 	
 	socket.on('chat_message_sent', function(msg){
 		io.emit('chat_message_received', { ...msg, user, from});
 	});
 
 	socket.on('disconnect', function(msg){
-		io.emit('member_exit', { from, user });
+		counter--;
+		io.emit('member_exit', { from, user, counter });
 	});
 
-	io.emit('new_member', { from, user });
+	io.emit('new_member', { from, user, counter });
+
+	socket.on('confetti_thrown', function() {
+		io.emit('confetti_received', { user, from });
+	});
 });
 
 http.listen(port, function(){
